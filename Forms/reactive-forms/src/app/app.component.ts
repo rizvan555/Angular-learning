6dbbf7be757b6afbc3a073e0f13d4cdbcffe17aa
Rizvan <rizvan@MacBook-Air-2.fritz.box>
2023-09-07 08:21:34 +0200

    created FORM for email and password, used disabled for activate button,used Validators for forms validation

---
+++ Forms/reactive-forms/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  addForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(10),
+    ]),
+  });
+
+  add() {
+    console.log(this.addForm);
+    this.addForm.reset(); //buttona baadiqdan sonra inputlarin icini bosaldir
+  }
+}
